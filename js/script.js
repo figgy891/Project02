1873c72a9a5c04091d5a5c337a788c44c54b69cf
figgy891 <figgy891@gmail.com>
2020-11-16 16:51:47 -0600

    udated jQuery file

--- js/script.js
+++ js/script.js
@@ -1,51 +1,15 @@
 $( document ).ready(function() {
   console.log( "ready!" );
 
-  // $('#search-field').on('change', () => {
-  //   console.log('input was changed!');
-  // });
-
-  // $('#search-field').change(() => {
-  //   console.log('input was changed!');
-  // });
-
-
-
-
-
   $('#search-button').on('click', () => {
-    // console.log('i was clicked');
-    // console.log($('#search-field').val());
     let searchTerm = $('#search-field').val();
-
     console.log(searchTerm);
-
     let url = "https://api.giphy.com/v1/gifs/search?api_key=4zv45wmD5vJvLn2UE3B4tNBY1ny9kZZV&q=" + searchTerm;
-
 
     // jquery way of doin it
     $.get(url, function(giphyResponse) {
-      // console.log(giphyResponse);
-      // console.log('giphyData', giphyResponse.data)
-      // console.log('giphyData result 0 (1st result)', giphyResponse.data[0]);
-      // console.log('giphyData result 0 (1st result) - all images list', giphyResponse.data[0].images);
-      // console.log('giphyData result 0 (1st result) - all images list - original image', giphyResponse.data[0].images.original);
-
-      // console.log('giphyData result 0 (1st result) - all images list - original image - url', giphyResponse.data[0].images.original.url);
-// 
-
       let resultsHtml = '';
-
-
-      // for(let image of response.images){
-      //   resultsHtml = resultsHtml + `<img src=${image.images.original.url} />`
-      // }
-
-      // $('#results').thml(resultsHtml)
-
-
       for (let image of giphyResponse.data) {                
-        //let imgHtml = `<img src=${image.images.original.url}/>`
         let imgHtml = `<img src=${image.images.original.url} class="col-12 col-sm-6 col-md-4 col-lg-3" style="padding: 2px"/>`
         resultsHtml = resultsHtml + imgHtml;          
       }
@@ -54,15 +18,6 @@ $( document ).ready(function() {
 
       $('#search-results').html(resultsHtml);
 
-
     });
-
-
-
-
-
   })
-
-
-
 });
